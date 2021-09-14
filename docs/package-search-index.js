60e35c377b67493277a0037e3186e30111be6ef5
Jacob Watters <47160062+jacobdwatters@users.noreply.github.com>
2021-09-14 14:43:52 -0600

    Add files via upload

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"benchmarks"},{"l":"linalg"},{"l":"linalg.complex_number"},{"l":"linalg.errors"},{"l":"linalg.util"},{"l":"unitTests"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"linalg"},{"l":"linalg.complex_number"},{"l":"linalg.util"}];updateSearchResults();
