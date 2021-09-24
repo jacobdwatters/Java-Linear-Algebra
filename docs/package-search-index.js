8d1505a2c04618e3ef7b4007205d6ca4882a26c3
jacobdwatters <jacobdwatters@gmail.com>
2021-09-24 17:02:38 -0600

    Update pom

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"linalg"},{"l":"linalg.complex_number"},{"l":"linalg.util"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.jacobwatters.linalg"},{"l":"com.github.jacobwatters.linalg.complex_number"},{"l":"com.github.jacobwatters.linalg.util"}];updateSearchResults();
